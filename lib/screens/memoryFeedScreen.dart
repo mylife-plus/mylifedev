4cbcba5bff80dca15d211adc0e9c6458039770d2
haythemd <drihmihaythem@gmail.com>
2024-12-21 00:27:56 +0100

    - integrating Sqlite Database - adding data models - adding repositories - remove draggable sheet - Populate MemoryScreen

--- lib/screens/memoryFeedScreen.dart
+++ lib/screens/memoryFeedScreen.dart
@@ -18,9 +18,72 @@ class MemoryFeedScreenState extends State<MemoryFeedScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius:
+        BorderRadius.vertical(top: Radius.circular(16)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 5,
+            offset: Offset(0, -2),
+          ),
+        ],
+      ),
+      child: ListView(
+        physics: const BouncingScrollPhysics(),
 
-    body: Container(),
+        children: [
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                MemoryContent(
+                  date: "24/12/2024, 15:30",
+                  country: "Country Name",
+                  reactions: 3,
+                  content:
+                  "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+                ),
+                const Divider(color: Colors.grey),
+                MemoryContent(
+                  date: "24/12/2024, 15:30",
+                  country: "Country Name",
+                  reactions: 3,
+                  content: "Lorem ipsum dolor sit amet...",
+                  imageUrls: [
+                    "https://cdn.builder.io/api/v1/image/assets/TEMP/7632e90dad2f4f0ca39a4830dbb1b01d72906e4c0ddc67d230681b967b7cc622?placeholderIfAbsent=true&apiKey=c43da3a161eb4f318c4f96480fdf0876",
+                    "https://cdn.builder.io/api/v1/image/assets/TEMP/7632e90dad2f4f0ca39a4830dbb1b01d72906e4c0ddc67d230681b967b7cc622?placeholderIfAbsent=true&apiKey=c43da3a161eb4f318c4f96480fdf0876",
+                  ],
+                ),
+                const Divider(color: Colors.grey),
+                MemoryContent(
+                  date: "24/12/2024, 15:30",
+                  country: "Country Name",
+                  reactions: 3,
+                  content:
+                  "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+                  imageUrls: [
+                    "https://cdn.builder.io/api/v1/image/assets/TEMP/7632e90dad2f4f0ca39a4830dbb1b01d72906e4c0ddc67d230681b967b7cc622?placeholderIfAbsent=true&apiKey=c43da3a161eb4f318c4f96480fdf0876",
+                    "https://cdn.builder.io/api/v1/image/assets/TEMP/7632e90dad2f4f0ca39a4830dbb1b01d72906e4c0ddc67d230681b967b7cc622?placeholderIfAbsent=true&apiKey=c43da3a161eb4f318c4f96480fdf0876",
+                  ],
+                ),
+                const Divider(color: Colors.grey),
+                MemoryContent(
+                  date: "24/12/2024, 15:30",
+                  country: "Country Name",
+                  reactions: 3,
+                  content:
+                  "i am the test dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+
     );
   }
 }
